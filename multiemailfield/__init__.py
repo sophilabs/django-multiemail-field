388983bce30cf2497a7a90f9c9cf7002a531c8ef
Pablo Ricco <pricco@gmail.com>
2013-11-05 18:55:23 -0200

    updated travis

--- multiemailfield/__init__.py
+++
@@ -1,1 +0,0 @@
-from multiemailfield.fields import MultiEmailField, MultiEmailFormField
--- multiemailfield/tests.py
+++ multiemailfield/tests.py
@@ -1,10 +1,5 @@
-import pytz
-
-from django import forms
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.test import TestCase
-from django.utils import six
 
 from . import MultiEmailField
 
